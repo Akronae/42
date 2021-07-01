87cb07ed3b9fd67e40d35716327dabdc97ad6349
Alexande Daubricourt <alexandre@daubricourt.com>
2021-07-01 15:19:20 +0200

    Update correction-generate-builds.bash

--- correction-generate-builds.bash
+++ correction-generate-builds.bash
@@ -18,9 +18,9 @@ for ex_dir in ex*; do
             if [ -f "$code_file" ]; then
                 FUNCTION_NAME="$(basename $code_file | cut -d. -f1)"
                 FILE_NAME="$FUNCTION_NAME.c"
-                FILE_CONTENT=$(echo $TEMPLATE | sed "s/{{function_name}}/$FUNCTION_NAME/g")
-                FILE_CONTENT=$(echo $FILE_CONTENT | sed "s/{{file_name}}/$FILE_NAME/g")
-                FILE_CONTENT=$(echo $FILE_CONTENT | sed "s/{{dir}}/$ex_dir/g")
+                FILE_CONTENT=$(echo "$TEMPLATE"  | sed "s/{{function_name}}/$FUNCTION_NAME/g")
+                FILE_CONTENT=$(echo "$FILE_CONTENT"  | sed "s/{{file_name}}/$FILE_NAME/g")
+                FILE_CONTENT=$(echo "$FILE_CONTENT"  | sed "s/{{dir}}/$ex_dir/g")
 
                 echo "$FILE_CONTENT" > $FILE_NAME
                 gcc -Wall -Werror -Wextra $FILE_NAME -o "$FUNCTION_NAME.out"
