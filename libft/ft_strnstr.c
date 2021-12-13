eb3174b0801b344b0b2834011eaa77761dcccdbf
Alexandre Daubricourt <alexandre@daubricourt.com>
2021-12-13 14:14:42 +0100

    m

---
+++ libft/ft_strnstr.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adaubric <adaubric@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/13 14:07:52 by adaubric          #+#    #+#             */
+/*   Updated: 2021/12/13 14:08:34 by adaubric         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, int len)
+{
+	int	i;
+	int	j;
+
+	if (!haystack && !needle && len)
+		return (NULL);
+	if (!needle || !needle[0])
+		return ((char *) haystack);
+	i = 0;
+	while (haystack[i] && i < len)
+	{
+		j = 0;
+		while (haystack[i + j] && needle[j] &&
+			   i + j < len && haystack[i + j] == needle[j])
+			j++;
+		if (!needle[j])
+			return ((char *)(haystack + i));
+		i++;
+	}
+	return (NULL);
+}
