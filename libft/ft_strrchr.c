97ca10489e8093f827278ffa6940fec45c96274e
Alexandre Daubricourt <alexandre@daubricourt.com>
2021-12-13 12:57:42 +0100

    m

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: adaubric <adaubric@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 12:23:05 by adaubric          #+#    #+#             */
-/*   Updated: 2021/12/13 12:33:09 by adaubric         ###   ########.fr       */
+/*   Updated: 2021/12/13 12:51:37 by adaubric         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ char	*ft_strrchr(const char *s, int c)
 {
 	int	i;
 
-	i = ft_strlen(s) - 1;
-	while (i >= 0 || c == '\0')
+	i = ft_strlen(s);
+	while (i >= 0)
 	{
 		if (s[i] == (char) c)
 			return ((char *)(s + i));
