01121f414c94ff7181263cef77915b2027e2353f
Alexandre DAUBRICOURT <adaubric@e3r11p19.clusters.42paris.fr>
2022-06-09 14:24:38 +0200

    m

--- push_swap/src/stacks_operations.c
+++ push_swap/src/stacks_operations.c
@@ -40,22 +40,6 @@ void	move_b_elem_to_top(t_stacks_op *op, size_t elem_index)
 	}
 }
 
-void	move_a_elem_to_bottom(t_stacks_op *op, size_t elem_index)
-{
-	int		move_up;
-	long	elem_val;
-
-	move_up = elem_index < op->stack_b->length / 2;
-	elem_val = op->a_at(op, elem_index);
-	while (op->a_at(op, 0) != elem_val)
-	{
-		if (move_up)
-			op->ra(op);
-		else
-			op->rra(op);
-	}
-}
-
 int	is_stack_ordered(t_list *stack)
 {
 	t_iterator	*i;
@@ -82,71 +66,4 @@ void	move_stack_b_to_stack_a(t_stacks_op *op)
 	{
 		op->pa(op);
 	}
-}
-
-long	get_greatest_lower_than(long lower_than, t_list *numbers)
-{
-	t_iterator	*i;
-	long long	greatest;
-
-	if (numbers->length == 0)
-		return (LONG_MAX);
-	i = numbers->get_iterator(numbers);
-	greatest = *numbers->find_min(numbers, T_TYPE_LONG)->data->as_long;
-	while (i->next(i))
-	{
-		if (*i->curr->data->as_long > greatest
-			&& *i->curr->data->as_long < lower_than)
-			greatest = *i->curr->data->as_long;
-	}
-	i->free(i);
-	return (greatest);
-}
-
-void	lol(t_stacks_op *op, t_list *biggest)
-{
-	t_typed_ptr	*typed_ptr;
-	t_link		*big;
-	long long	greatest;
-	long 		index;
-
-	while (biggest->length > 0)
-	{
-		big = biggest->find(biggest, op->stack_a->get_elem(op->stack_a, -1)->data);
-		if (big)
-		{
-			greatest = get_greatest_lower_than(*big->data->as_long,
-					op->stack_b);
-			typed_ptr = new_typed_ptr_decimal(greatest);
-			index = op->stack_b->find_index(op->stack_b, typed_ptr);
-			typed_ptr->free(typed_ptr);
-			if (index != INDEX_NOT_FOUND)
-				move_b_elem_to_top(op, index);
-			if (greatest > *big->data->as_long && op->stack_b->length > 1)
-				op->rb(op);
-			op->pb(op);
-			biggest->free_by_data(biggest, big->data);
-		}
-		else
-			op->ra(op);
-	}
-}
-
-void	move_biggest_range_to_b(t_stacks_op *op, long from, long to)
-{
-	t_list		*biggest;
-	t_typed_ptr	*typed_ptr;
-	t_list		*sorted;
-	t_list		*d;
-
-	sorted = op->stack_a->sort(op->stack_a, T_TYPE_LONG);
-	d = sorted->reverse(sorted);
-	biggest = d->sub(d, from, to);
-	lol(op, biggest);
-	typed_ptr = new_typed_ptr_decimal(*op->stack_b
-			->find_max(op->stack_b, T_TYPE_LONG)->data->as_long);
-	move_b_elem_to_top(op, op->stack_b->find_index(op->stack_b, typed_ptr));
-	typed_ptr->free(typed_ptr);
-	biggest->free(biggest);
-	d->free(d);
 }
